1dd8e084bc974c81ad9e04c7cd96b0738980f6da
Kohei Narumi <kurobee5436@gmail.com>
2025-04-30 23:18:25 +0900

    画像取り込み＆フェードイン・アウト

--- src/laravelapp/resources/views/welcome.blade.php
+++ src/laravelapp/resources/views/welcome.blade.php
@@ -4,6 +4,17 @@
 @section('title', 'Home')
 
 @section('content')
+    <div class="carousel">
+        <div class="slides">
+            <div class="slide slide1">
+                <img src="{{ asset('images/autumn_kodaiji.jpg') }}" alt="Photo 1">
+            </div>
+            <div class="slide slide2">
+                <img src="{{ asset('images/kingyo_aquarium.jpg') }}" alt="Photo 2">
+            </div>
+        </div>
+    </div>
+
     <h2>ようこそ！</h2>
     <p>ここはnakoが自由に書き連ねる場所です。</p>
 @endsection
